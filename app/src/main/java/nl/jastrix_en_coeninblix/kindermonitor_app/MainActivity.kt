c5a1943e2bb7723d3eb35c8eb760e4b654701eaf
Cuenity <coenengelbracht@gmail.com>
2019-12-02 21:29:14 +0100

    Begin van navigatie

--- app/src/main/java/nl/jastrix_en_coeninblix/kindermonitor_app/MainActivity.kt
+++ app/src/main/java/nl/jastrix_en_coeninblix/kindermonitor_app/MainActivity.kt
@@ -4,6 +4,17 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import nl.jastrix_en_coeninblix.kindermonitor_app.api.APIService
 import nl.jastrix_en_coeninblix.kindermonitor_app.login.LoginActivity
 import retrofit2.Retrofit
@@ -15,22 +26,58 @@ class MainActivity : AppCompatActivity() {
         var authToken: String? = ""
     }
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        // uncomment next line after testing login and register
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+//         // uncomment next line after testing login and register
 //        authToken = getSharedPreferences("kinderMonitorApp", Context.MODE_PRIVATE).getString("AuthenticationToken", "")
+//
+//        if (authToken != "" && authToken != null)
+//        {
+//            initHomeActivity();
+//        }
+//        else
+//        {
+//            val intent: Intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
 
-        if (authToken != "" && authToken != null)
-        {
-            initHomeActivity();
-        }
-        else
-        {
-            val intent: Intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
     }
 
     private fun initHomeActivity() {
