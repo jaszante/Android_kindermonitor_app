ee71a776ad737d4164586a94dfdb07890f7f0830
jaszante <jaszante@gmail.com>
2019-12-06 12:08:47 +0100

    register kind pagina

---
+++ app/src/main/java/nl/jastrix_en_coeninblix/kindermonitor_app/Register/RegisterPatientActivity.kt
@@ -0,0 +1,13 @@
+package nl.jastrix_en_coeninblix.kindermonitor_app.Register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.jastrix_en_coeninblix.kindermonitor_app.R
+
+class RegisterPatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_patient)
+    }
+}
