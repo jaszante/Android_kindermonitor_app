36c9dca7eccb5fced296cb91626e222f3554177f
jaszante <jaszante@gmail.com>
2019-12-15 23:10:49 +0100

    Added graphpage en graph view

---
+++ app/src/main/java/nl/jastrix_en_coeninblix/kindermonitor_app/Account/AccountPage.kt
@@ -0,0 +1,13 @@
+package nl.jastrix_en_coeninblix.kindermonitor_app.Account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.jastrix_en_coeninblix.kindermonitor_app.R
+
+class AccountPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_page)
+    }
+}
